e7d2250268f0449d19e080a229e0147a0135a937
wind <aa.veter@gmail.com>
2016-05-31 20:51:47 +0300

    +1

--- 9/try_log.py
+++ 9/try_log.py
@@ -2,15 +2,26 @@
 # Декоратор для логирования
 from datetime import datetime
 
+
+def do_log(func_name, tip='start'):
+    with open('log.txt', 'a') as f:
+        f.write('{}: {} {}\n'.format(
+            datetime.now(),  # текущее время
+            tip,
+            func_name  # имя запускаемой функции
+        ))
+
 def logging(func):
 
     def new_func(*args, **kwargs):
-        with open('log.txt', 'a') as f:
-            f.write('{}: {}\n'.format(
-                datetime.now(),        # текущее время
-                func.__name__          # имя запускаемой функции
-            ))
-        return func(*args, **kwargs)
+
+        do_log(func.__name__) # имя запускаемой функции
+
+        ret = func(*args, **kwargs)
+
+        do_log(func.__name__, 'end')
+
+        return ret
 
     return new_func
 
