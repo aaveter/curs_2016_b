047426ecb7c35629a961451cedca148a8b296ed0
wind <aa.veter@gmail.com>
2016-06-04 18:59:45 +0300

    +1

---
+++ 11/try_sockets.py
@@ -0,0 +1,25 @@
+
+
+import socket
+
+# Создаем объект сокета
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Устанавливаем таймаут
+s.settimeout(3)
+
+# Соединяемся с сервером
+s.connect(("www.python.org", 80))
+
+
+data = b'''GET / HTTP/1.1
+
+'''
+
+# Отправляем
+s.sendall(data)
+
+# Читаем данные
+data = s.recv(100)
+
+print( data )
