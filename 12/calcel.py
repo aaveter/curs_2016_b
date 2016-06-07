c16a5600e7b0ee5a7210961e25a6e9605119c8e3
wind <aa.veter@gmail.com>
2016-06-07 19:45:26 +0300

    +1

--- 12/calcel.py
+++ 12/calcel.py
@@ -10,6 +10,11 @@ w = QtGui.QWidget() # пустой виджет ( = форма = окно )
 
 uic.loadUi('calcer.ui', w)
 
+def on_1():
+    w.lineEdit.setText('1')
+
+w.but_1.clicked.connect(on_1)
+
 w.show() # Отобразить
 w.raise_() # для мака в PyQt4
 
